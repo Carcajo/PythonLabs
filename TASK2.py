77ddabf84f3d9af08b8a66d5a2c0e5ef76b28c9c
Carcajo <t375445391507@gmail.com>
2023-02-27 19:19:15 +0300

    Add task2

---
+++ TASK2.py
@@ -0,0 +1,33 @@
+def get_float_input(prompt):
+    while True:
+        try:
+            user_input = input(prompt)
+            float_input = float(user_input)
+            return float_input
+        except ValueError:
+            print("Error. Try input digit")
+
+
+def calculate(num1, num2, operation):
+    if operation == 'add':
+        print(num1 + num2)
+    elif operation == 'sub':
+        print(num1 - num2)
+    elif operation == 'mult':
+        print(num1 * num2)
+    elif operation == 'div':
+        if num2 == 0:
+            print("Error: division by zero")
+            return None
+        else:
+            print(num1 / num2)
+    else:
+        print("Error: invalid operation")
+        return None
+
+
+number1 = get_float_input("Input firs number: ")
+number2 = get_float_input("Input firs number: ")
+action = input()
+
+calculate(number1, number2,  action)
