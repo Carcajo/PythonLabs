8fcd803a5e0b69ece9b12ec4c6bfdc0d7e67363e
fridrixjo <pglutov@gmail.com>
2023-05-18 23:10:12 +0300

    change styles

--- gshop/models.py
+++ gshop/models.py
@@ -6,14 +6,14 @@ from django.urls import reverse
 
 
 class Product(models.Model):
-    name = models.CharField(max_length=255, verbose_name="Наименование продукта")
-    slug = models.SlugField(max_length=255, db_index=True, verbose_name="URL", unique=True)
-    description = models.TextField(verbose_name="Описание продукта")
-    price = models.FloatField(verbose_name="Цена")
-    photo = models.ImageField(upload_to=f"photos/", verbose_name="Фото")
-    time_create = models.DateTimeField(auto_now_add=True, verbose_name="Время создания")
-    is_available = models.BooleanField(default=True, verbose_name="В наличии")
-    category = models.ForeignKey("Category", on_delete=models.PROTECT, verbose_name="Категория")
+    name = models.CharField(max_length=255, verbose_name="Product name")
+    slug = models.SlugField(max_length=255, db_index=True, verbose_name="Unique Identifier", unique=True)
+    description = models.TextField(verbose_name="Product Description")
+    price = models.FloatField(verbose_name="Price")
+    photo = models.ImageField(upload_to=f"photos/", verbose_name="Picture")
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name="Creation date")
+    is_available = models.BooleanField(default=True, verbose_name="In stock")
+    category = models.ForeignKey("Category", on_delete=models.PROTECT, verbose_name="Category")
 
     def __str__(self):
         return self.name
@@ -22,14 +22,14 @@ class Product(models.Model):
         return reverse('product', kwargs={"product_slug": self.slug, "category_slug": self.category.slug})
 
     class Meta:
-        verbose_name = "Продукты"
-        verbose_name_plural = "Продукты"
+        verbose_name = "Products"
+        verbose_name_plural = "Products"
         ordering = ['name']
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=180, db_index=True, verbose_name="Тип продукта")
-    slug = models.SlugField(max_length=255, db_index=True, verbose_name="URL", unique=True)
+    name = models.CharField(max_length=180, db_index=True, verbose_name="Product type")
+    slug = models.SlugField(max_length=255, db_index=True, verbose_name="Unique Identifier", unique=True)
 
     def __str__(self):
         return self.name
@@ -38,18 +38,18 @@ class Category(models.Model):
         return reverse('category', kwargs={"category_slug": self.slug})
 
     class Meta:
-        verbose_name = "Тип продукта"
-        verbose_name_plural = "Типы продуктов"
+        verbose_name = "Product type"
+        verbose_name_plural = "Product types"
         ordering = ['name']
 
 
 class Promotion(models.Model):
-    title = models.CharField(max_length=180, verbose_name="Название акции")
-    text = models.TextField(verbose_name="Текст")
-    time_create = models.DateTimeField(auto_now_add=True, verbose_name="Время публикации")
-    send_now = models.BooleanField(default=True, verbose_name="Отправить email всем покупателям")
+    title = models.CharField(max_length=180, verbose_name="Share name")
+    text = models.TextField(verbose_name="Text")
+    time_create = models.DateTimeField(auto_now_add=True, verbose_name="Publication time")
+    send_now = models.BooleanField(default=True, verbose_name="Send email to all buyers")
 
     class Meta:
-        verbose_name = "Специальное предложение"
-        verbose_name_plural = "Специальные предложения"
+        verbose_name = "Special offer"
+        verbose_name_plural = "Special offers"
         ordering = ['time_create']
