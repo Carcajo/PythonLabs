e47be9109e29a99a491778be7ea96817dcb287db
fridrixjo <pglutov@gmail.com>
2023-05-19 11:04:56 +0300

    add tests to user

--- cart/test_views.py
+++ cart/test_views.py
@@ -8,21 +8,21 @@ from gshop.models import Product, Category
 
 class TestCart(APITestCase):
     def test_cart_add_no_login_post(self):
-        cat = Category.objects.create(name="pizza", slug="pizza")
-        prod = Product.objects.create(name="vetchina", slug="vetchina", description="test", price=123.0, category=cat)
+        food = Category.objects.create(name="food", slug="dinner")
+        prod = Product.objects.create(name="pasta", slug="karbonara", descrition="ahfjskdl", price=123.0, category=food)
         url = reverse_lazy('cart_add', args=(prod.pk,))
         code = self.client.post(url).status_code
         self.assertEqual(status.HTTP_302_FOUND, code)
 
     def test_add_to_cart_login_post(self):
         # create cat and product
-        cat = Category.objects.create(name="pizza", slug="pizza")
-        prod = Product.objects.create(name="vetchina", slug="vetchina", description="test", price=123.0, category=cat)
+        food = Category.objects.create(name="food", slug="dinner")
+        prod = Product.objects.create(name="pasta", slug="karbonara", description="ahfjskdl", price=123.0, category=food)
 
         # create and login user
         url = reverse_lazy('login')
-        User.objects.create_user({"username": "testbro", "password": "blahblahblah"})
-        self.client.post(url, {"username": "testbro", "password": "blahblahblah"})  # user login
+        User.objects.create_user({"username": "user", "password": "pass1234"})
+        self.client.post(url, {"username": "user", "password": "pass1234"})  # user login
 
         # making order
         url = reverse_lazy('cart_add', args=(prod.pk,))
